74f4800899336b99294b39a07cab46832a04ea63
Softwarica College Lab <softwarica@softwarica.com>
2019-05-22 11:51:56 +0545

    image laod from internal storage

--- app/src/main/java/url/Url.java
+++ app/src/main/java/url/Url.java
@@ -1,6 +1,18 @@
 package url;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class Url {
 
     public static final String BASE_URL = "http://10.0.2.2:3000";
+
+    public static Retrofit getInstance(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Url.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        return retrofit;
+    }
 }
