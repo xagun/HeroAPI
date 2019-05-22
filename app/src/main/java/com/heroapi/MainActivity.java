8d03d89d701516c2618fc402cc2b1cbd21271710
Softwarica College Lab <softwarica@softwarica.com>
2019-05-22 11:25:37 +0545

    image laod from internal storage

--- app/src/main/java/com/heroapi/MainActivity.java
+++ app/src/main/java/com/heroapi/MainActivity.java
@@ -114,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
         cursor.close();
         return result;
     }
+//
 //    private void StrictMode() {
 //        android.os.StrictMode.ThreadPolicy policy = new android.os.StrictMode.ThreadPolicy.Builder().permitAll().build();
 //        android.os.StrictMode.setThreadPolicy(policy);
