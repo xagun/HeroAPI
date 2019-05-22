c3bcde3cb201528df0d5ff51ec7ce16d77caa2db
Softwarica College Lab <softwarica@softwarica.com>
2019-05-22 12:01:54 +0545

    image laod from internal storage

--- app/src/main/java/heroapi/HeroAPI.java
+++ app/src/main/java/heroapi/HeroAPI.java
@@ -1,11 +1,20 @@
 package heroapi;
 
+import java.util.List;
+import java.util.Map;
+
 import model.Heroes;
+import model.ImageResponse;
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface HeroAPI {
 
@@ -14,7 +23,18 @@ public interface HeroAPI {
 
     @FormUrlEncoded
     @POST("heroes")
-    Call<Void> addHero(@Field("name") String name, @Field("desc") String desc)l
+    Call<Void> addHero(@Field("name") String name, @Field("desc") String desc);
+
+    @FormUrlEncoded
+    @POST("heroes")
+    Call<Void> addHero(@FieldMap Map<String,String> map);
+
+    @Multipart
+    @POST("upload")
+    Call<ImageResponse> uploadImage(@Part MultipartBody.Part img);
+
+    @GET("heroes")
+    Call<List<Heroes>> getAllHeroes();
 
 
 }
---
+++ app/src/main/java/model/ImageResponse.java
@@ -0,0 +1,8 @@
+package model;
+
+public class ImageResponse {
+    private String filename;
+    public String getFilename(){
+        return filename;
+    }
+}
