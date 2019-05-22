7b019a3c633a7f9bf583257e6f087f46b282a438
Softwarica College Lab <softwarica@softwarica.com>
2019-05-22 11:55:16 +0545

    image laod from internal storage

--- app/src/main/java/heroapi/HeroAPI.java
+++ app/src/main/java/heroapi/HeroAPI.java
@@ -3,11 +3,18 @@ package heroapi;
 import model.Heroes;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface HeroAPI {
 
     @POST("heroes")
     Call<Void> addHero(@Body Heroes heroes);
+
+    @FormUrlEncoded
+    @POST("heroes")
+    Call<Void> addHero(@Field("name") String name, @Field("desc") String desc)l
+
 
 }
